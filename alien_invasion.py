ce2bd2a010b4fccf06d42fea3099db6853a011cf
EusebiFos46 <eufosan@alumni.uv.es>
2026-05-16 20:13:26 +0200

    Refactor AlienInvasion class to use Settings for screen dimensions and background color

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,18 +1,18 @@
 import sys
 import pygame
+from settings import Settings
+
 
 class AlienInvasion:
     "General class for managing game resources and behavior"
     def __init__(self):
         pygame.init()
         self.clock= pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        # Set the background color
-        self.bg_color = (230, 230, 230)
-
-
+        
     def run_game(self):
         "Start the main loop for the game"
         while True:
@@ -22,7 +22,7 @@ class AlienInvasion:
                     sys.exit()
             
             # Redraws the screen at each step through the loop
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             # makes the last drawn screen visible
             pygame.display.flip()
 
