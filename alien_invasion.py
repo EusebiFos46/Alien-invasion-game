a206e099bd5928f58a4f2c7124811e04e67f714e
EusebiFos46 <eufosan@alumni.uv.es>
2026-05-16 20:05:04 +0200

    Refactor AlienInvasion class to initialize clock and background color, and update game loop for screen redraw

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,8 +5,13 @@ class AlienInvasion:
     "General class for managing game resources and behavior"
     def __init__(self):
         pygame.init()
+        self.clock= pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+        # Set the background color
+        self.bg_color = (230, 230, 230)
+
 
     def run_game(self):
         "Start the main loop for the game"
@@ -15,8 +20,13 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            # Redraws the screen at each step through the loop
+            self.screen.fill(self.bg_color)
             # makes the last drawn screen visible
             pygame.display.flip()
+
+            self.clock.tick(60) # limits the game to 60 frames per second
     
 if __name__ == '__main__':
     # make a game instance and run the game
