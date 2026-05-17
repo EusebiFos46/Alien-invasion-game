c0b1acb79bed21ac20137a2bcd189040274420ad
EusebiFos46 <eufosan@alumni.uv.es>
2026-05-17 12:46:21 +0200

    Fix ship movement logic in event handling

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,6 +20,14 @@ class AlienInvasion:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame .K_RIGHT:
+                        # Move the ship to the right
+                        self.ship.rect.x += 1
+                    elif event.key == pygame.K_LEFT:
+                        # Move the ship to the left
+                        self.ship.rect.x == -1
+                            
     
     def _update_screen(self):
          "Upadate images on the screen and flip to the new screen"
