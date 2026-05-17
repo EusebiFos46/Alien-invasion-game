8c32e04962f5d4de4acca67997261b4bb67bc92d
EusebiFos46 <eufosan@alumni.uv.es>
2026-05-17 20:14:39 +0200

    Limit bullet creation to the maximum allowed and refactor bullet update logic

--- alien_invasion.py
+++ alien_invasion.py
@@ -59,8 +59,16 @@ class AlienInvasion:
     
     def fire_bullet(self):
         "Create a new bullet and add it to the bullets group"
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        "Update position of bullets and get rid of old bullets"
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
          "Upadate images on the screen and flip to the new screen"
@@ -76,13 +84,8 @@ class AlienInvasion:
         "Start the main loop for the game"
         while True:
             self._check_events()   
-            self.ship.update()   
-             
-            self.bullets.update()   
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-
+            self.ship.update()    
+            self._update_bullets()
             self._update_screen()            
             self.clock.tick(60) # limits the game to 60 frames per second
     
