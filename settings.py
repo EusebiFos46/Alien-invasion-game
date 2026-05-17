c71eebec4360966d91aa51e5edcd6c34911e6bbf
EusebiFos46 <eufosan@alumni.uv.es>
2026-05-17 20:08:13 +0200

    Add bullet functionality for the ship and update settings for bullets

--- settings.py
+++ settings.py
@@ -1,3 +1,4 @@
+
 class Settings:
     """A class to store all settings for Alien Invasion."""
     def __init__(self):
@@ -7,6 +8,12 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 2
-    
+
+        # Bullets settings
+        self.bullet_speed = 2
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
     
 
